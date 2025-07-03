9be763754e06b5629e0b22fa4e64423496819442
satyam1708 <pandeysatyam1708@gmail.com>
2025-07-04 00:33:30 +0530

    Port

--- index.js
+++ index.js
@@ -30,4 +30,5 @@ app.get("/", (req, res) => {
 
 // === Start Server ===
 const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
