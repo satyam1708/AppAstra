9e3bb99d604ef4b3c4fb70ec1ead517f33e034e2
satyam1708 <pandeysatyam1708@gmail.com>
2025-08-09 18:16:51 +0530

    Updated

--- routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { requestOTP, verifyOTP, resendOTP } = require("../controllers/authController");
-
-router.post("/request-otp", requestOTP);
-router.post("/verify-otp", verifyOTP);
-router.post("/resend-otp", resendOTP); 
-
-module.exports = router;
